b17c18df9cc1b157bf7781d44ea47502ce2dcb21
Maneesh Sreekumar <c-msreekumar@hyphensolutions.com>
2024-07-10 07:41:44 +0530

    React useEffect

---
+++ useEffect/use-effect/src/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+
+function Counter() {
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
+  useEffect(() => {
+      console.log('Mounting...');
+      console.log('Count...'+count);
+      console.log('Count2...'+count2);
+      
+  }, [count, count2])
+  return (
+    <div>
+        <button onClick={() => setCount(count+1)}>Increment</button>
+        <h1>Hello Component: {count}</h1>
+        <button onClick={() => setCount2(count2+1)}>Increment</button>
+        <h1>Hello Component 2: {count2}</h1>
+    </div>
+  )
+}
+
+export default Counter
